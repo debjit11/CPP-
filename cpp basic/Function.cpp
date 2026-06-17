f21b9d598b24c31055543f1cd15022855bc7551b
Debjit <ddebjit421@gmail.com>
2026-06-17 20:30:26 +0530

    gitignore file add

--- cpp basic/Function.cpp
+++ cpp basic/Function.cpp
@@ -3,25 +3,25 @@
 // int Sum (int a,int b){
 //     return a + b ;
 // }
-// // int function(NON Void function)
-// // Returns a value (integer)
-// // You can store or use the result
+// int function(NON Void function)
+// Returns a value (integer)
+// You can store or use the result
 // void printmyname(){
 //  for (int i= 1; i <=5; i ++ ){
 //     cout << "Debjit" << endl;
 //  }
-// // }
-// // void function 
-// // Does NOT return anything
-// // It just performs an action
-// // int celcious(int num){
-// //     return (num - 32)*5/9;
-// // }
+// }
+// void function 
+// Does NOT return anything
+// It just performs an action
+// int celcious(int num){
+//     return (num - 32)*5/9;
+// }
 // int main(){
 
 //     int sum = Sum(5,9);
 //     cout << sum <<endl;
-//     // printmyname();
+    // printmyname();
 //     float calculation = celcious(32);
 //     cout << calculation <<endl;
 //     return 0;
