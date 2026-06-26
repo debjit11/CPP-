dade592449f125f7d77978767d804bd13ccac7e4
Debjit <ddebjit421@gmail.com>
2026-06-27 02:16:22 +0530

    STL vector

---
+++ cpp STL/vector.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <vector>
+using namespace std ;
+
+int main(){
+ vector <int> marks;
+
+ marks.push_back(10);
+ marks.push_back(20);
+ marks.push_back(30);
+//  cout << *(marks.begin()) <<" "<<  endl;
+ cout << marks.size() <<  endl;
+
+}
