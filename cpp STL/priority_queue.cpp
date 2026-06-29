ce3e19f1e7672cf12b98d91b8c8ed124ea3e7c49
Debjit <ddebjit421@gmail.com>
2026-06-30 02:29:45 +0530

    priority_queue

---
+++ cpp STL/priority_queue.cpp
@@ -0,0 +1,64 @@
+#include<iostream>
+#include<queue>
+using namespace std;
+
+int main() {
+
+    //min-heap -> Minimum Value -> Highest Priority
+    priority_queue<int,vector<int>, greater<int> > pq;
+
+    pq.push(100);
+    //100
+    pq.push(50);
+    //50,100
+    pq.push(75);
+    //50,75,100
+
+    cout << pq.top() << endl;
+    pq.pop();
+    //75,100
+    cout << pq.top() << endl;
+    pq.pop();
+    //100
+
+
+
+
+    //creation
+    // priority_queue<int> pq;
+    //max-heap -> maximum value -> Highest Priority
+
+    // pq.push(10);
+   
+    // pq.push(25);
+   
+    // pq.push(55);
+    
+    // pq.push(21);
+  
+
+    //top element -> Highest priority element
+    // cout << pq.top() << endl;
+    //55
+    // pq.pop();
+    //highest priority elemnet -> pop
+    //i.e -> 55 ko pop krdia 
+    // cout << pq.top() << endl;
+    // pq.pop();
+
+    
+    // cout << pq.top() << endl;
+
+    // cout << pq.size() << endl;
+
+    // if(pq.empty() == true) {
+    //     cout << "PQ is empty" << endl;
+    // }
+    // else {
+    //     cout << "PQ is not empty" << endl;
+    // }
+
+
+
+    return 0;
+}
