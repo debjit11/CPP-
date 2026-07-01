1f18a9ae7410718c741b87ea7c604a6b64f73f43
Debjit <ddebjit421@gmail.com>
2026-07-01 12:04:34 +0530

    search and finding algorithms

---
+++ cpp STL/searching_findingalgo.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+using namespace std ;
+int main () {
+    vector <int> arr ;
+    arr.push_back(10);
+    arr.push_back(20);
+    arr.push_back(30);
+    arr.push_back(40);
+    arr.push_back(50);
+    arr.push_back(60);
+// int target = 40;
+//     bool it = binary_search(arr.begin(),arr.end(),target);
+//     cout << it << endl ;
+
+// auto it = lower_bound(arr.begin(), arr.end(),31);
+//     cout << *it << endl ;
+
+// auto it = upper_bound(arr.begin(), arr.end(),35);
+//     cout << *it << endl ;
+// vector<int> arr1 = {10, 20, 30, 35, 35, 35, 40, 50};
+
+// auto it = equal_range(arr1.begin(), arr1.end(),35); // returns both the lower bound and upper bound in a single function call.
+//     cout << "Lower Bound: " << *it.first << endl ;
+//     cout << "Upper Bound: " <<  *it.second << endl ;
+}
